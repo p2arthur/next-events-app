4f12a316d8baac7aa5670e266e02cea0b38f6ef9
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-07-31 16:10:16 -0300

    create routes for our events app

---
+++ pages/events/[...slug].jsx
@@ -0,0 +1,9 @@
+const EventsByDate = () => {
+    return (
+        <div>
+            <h1>Events by date</h1>
+        </div>
+    );
+};
+
+export default EventsByDate;
---
+++ pages/events/index.jsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+const AllEventsPage = () => {
+    return (
+        <div><h1>All Events Page</h1></div>
+    )
+}
+
+export default AllEventsPage
